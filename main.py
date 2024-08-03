a275cb17a9449a5004e3c8b2f370e00885059b22
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2024-08-03 11:43:41 +0200

    Scheduling of charging now added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from typing import AsyncIterator
+import datetime as dt
 
 from pyeasee import Easee
 import argparse
@@ -9,6 +10,7 @@ from pyeasee.charger import STATUS as CHARGER_STATUS, Charger
 
 from ladning.charging_plan import create_charging_plan
 from ladning.energy_prices import get_energy_prices
+from ladning.types import ChargingPlan
 from ladning.vehicle_query import get_vehicle_charge_state
 
 
@@ -35,6 +37,19 @@ async def listen_for_charging_states(easee: Easee, charger: Charger) -> AsyncIte
         yield await queue.get()
 
 
+async def schedule_charge(charger: Charger, charging_plan: ChargingPlan) -> None:
+    def _format(d: dt.datetime):
+        # TODO: Convert to UTC nicely here - required by Easee API
+        return (d - dt.timedelta(hours=2)).isoformat(timespec='milliseconds') + "Z"
+
+    response = await charger.set_basic_charge_plan(id=42,  # Unsure what ID to use here
+                                                   chargeStartTime=_format(charging_plan.start_time),
+                                                   chargeStopTime=_format(charging_plan.end_time),
+                                                   repeat=False)
+    if not response.ok:
+        raise RuntimeError(f"Scheduling charge failed: '{response.reason}' (code {response.status})")
+
+
 async def smart_charge(easee: Easee) -> None:
     # Find the one charger that we intend to control/listen to
     chargers = await easee.get_chargers()
@@ -43,11 +58,12 @@ async def smart_charge(easee: Easee) -> None:
     charger = chargers[0]
 
     async for new_charging_state in listen_for_charging_states(easee, charger):
-        if new_charging_state == "AWAITING_START":
+        if new_charging_state == "AWAITING_START" or new_charging_state == "READY_TO_CHARGE":
             charge_state = get_vehicle_charge_state(allow_wakeup=True)
             hourly_prices = get_energy_prices()
             charging_plan = create_charging_plan(charge_state, hourly_prices)
             print(f"New charging plan created: {charging_plan}")
+            await schedule_charge(charger, charging_plan)
         elif new_charging_state == "DISCONNECTED":
             print("Vehicle not connected to charger - awaiting connection")
 
