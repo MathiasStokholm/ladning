4704dd630ef2e48b7891067fd5597cad79d306f1
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2024-08-04 21:14:05 +0200

    Cleaned up conditions for scheduling of charging

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import AsyncIterator
+from typing import AsyncIterator, Tuple, Optional
 import datetime as dt
 
 from pyeasee import Easee
@@ -14,13 +14,13 @@ from ladning.types import ChargingPlan
 from ladning.vehicle_query import get_vehicle_charge_state
 
 
-async def listen_for_charging_states(easee: Easee, charger: Charger) -> AsyncIterator[str]:
+async def listen_for_charging_states(easee: Easee, charger: Charger) -> AsyncIterator[Tuple[Optional[str], str]]:
     queue = asyncio.Queue()
 
     # Query the current charger mode
     current_charging_state: str = (await charger.get_state())["chargerOpMode"]
     print(f"Initial charging state: {current_charging_state}")
-    yield current_charging_state
+    yield None, current_charging_state
 
     async def _signalr_callback(_, __, data_id, value):
         if pyeasee.ChargerStreamData(data_id) == pyeasee.ChargerStreamData.state_chargerOpMode:
@@ -29,8 +29,8 @@ async def listen_for_charging_states(easee: Easee, charger: Charger) -> AsyncIte
             nonlocal current_charging_state
             if new_charging_state != current_charging_state:
                 print(f"New charging state: {new_charging_state}")
+                await queue.put((current_charging_state, new_charging_state))
                 current_charging_state = new_charging_state
-                await queue.put(new_charging_state)
 
     await easee.sr_subscribe(charger, _signalr_callback)
     while True:
@@ -58,16 +58,29 @@ async def smart_charge(easee: Easee) -> None:
         raise RuntimeError(f"Expected a single charger, got {len(chargers)}")
     charger = chargers[0]
 
-    async for new_charging_state in listen_for_charging_states(easee, charger):
-        if new_charging_state == "AWAITING_START" or new_charging_state == "READY_TO_CHARGE" or new_charging_state == "CHARGING":
-            charge_state = get_vehicle_charge_state(allow_wakeup=True)
-            hourly_prices = get_energy_prices()
-            charging_plan = create_charging_plan(charge_state, hourly_prices)
-            print(f"New charging plan created: {charging_plan}")
-            await schedule_charge(charger, charging_plan)
-            print(f"New charging plan scheduled")
-        elif new_charging_state == "DISCONNECTED":
+    async for previous_state, new_state in listen_for_charging_states(easee, charger):
+        if new_state == "DISCONNECTED":
             print("Vehicle not connected to charger - awaiting connection")
+            continue
+
+        # If previous state was None (app just started) or disconnected, consider whether to perform planning
+        if previous_state is None or previous_state == "DISCONNECTED":
+            # Plan if charger is ready to charge, awaiting a schedule or already started charging
+            perform_planning = new_state == "READY_TO_CHARGE" or \
+                               new_state == "AWAITING_START" or \
+                               new_state == "CHARGING"
+
+            if perform_planning:
+                charge_state = get_vehicle_charge_state(allow_wakeup=True)
+                hourly_prices = get_energy_prices()
+                charging_plan = create_charging_plan(charge_state, hourly_prices)
+                if charging_plan is None:
+                    print(f"Car already at target battery level, no plan will be scheduled")
+                    continue
+
+                print(f"New charging plan created: {charging_plan}")
+                await schedule_charge(charger, charging_plan)
+                print(f"New charging plan scheduled")
 
 
 def main():
