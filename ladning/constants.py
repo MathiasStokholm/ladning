1aded0f52282dcec78a439d43801ed53764c2e60
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2024-08-10 10:33:36 +0200

    Changed charging rate from ideal 11.0 to observed 10.6

--- ladning/constants.py
+++ ladning/constants.py
@@ -1,3 +1,3 @@
 MILES_TO_KILOMETERS = 1.609344
 BATTERY_CAPACITY_KWH = 57.5  # Tesla Model 3 Highland RWD
-CHARGING_KW = 11.0  # Easee Lite
+CHARGING_KW = 10.6  # Observed Easee Lite realistic charging speed
