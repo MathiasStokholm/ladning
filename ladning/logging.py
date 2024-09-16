53b6a65e0c4b273bf4735ff621493713b0311b3d
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2024-09-16 20:01:55 +0200

    Also forward PyEasee debug output to log

--- ladning/logging.py
+++ ladning/logging.py
@@ -7,3 +7,8 @@ log.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter("%(asctime)s (%(levelname)s): %(message)s", datefmt="%Y-%m-%d %H:%M:%S"))
 log.addHandler(handler)
+
+# Also log output from PyEasee for debugging purposes
+pyeasee_logger = logging.getLogger("pyeasee")
+pyeasee_logger.setLevel(logging.DEBUG)
+pyeasee_logger.addHandler(handler)
