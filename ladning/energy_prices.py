1ec200d9b369709c58dc306b98159049de416c0d
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2025-02-08 13:30:38 +0100

    Switched energy price provider to elprisen.somjson.dk

--- ladning/energy_prices.py
+++ ladning/energy_prices.py
@@ -14,24 +14,25 @@ def next_datetime_at_hour(current: dt.datetime, hour: int, minutes: int = 0) -> 
 
 def get_energy_prices() -> List[HourlyPrice]:
     """
-    Get the energy prices including tariffs and taxes from Bolius.
+    Get the energy prices including tariffs and taxes from https://elprisen.somjson.dk.
 
-    See https://www.bolius.dk/elpriser for more info.
+    See https://elprisen.somjson.dk/apidocs/#/Elpriser/get_elpris for more info.
 
     :return: The hourly energy prices from now until the most recently published price
     """
-    endpoint = "https://api.bolius.dk/livedata/v2/type/power"
-    price_area = "DK2"  # Price area for Sealand and Copenhagen
-    date_start_str = dt.datetime.now().astimezone().strftime("%Y-%m-%dT%H:00")
-    url = f"{endpoint}?region={price_area}&start={date_start_str}"
-    records = requests.get(url).json()["data"]
+    radius_elnet_gln_number = "5790000705689"  # Price area for Sealand and Copenhagen
+    url = f"https://elprisen.somjson.dk/elpris?GLN_Number={radius_elnet_gln_number}"
+    records = requests.get(url).json()["records"]
 
     def _convert(record: Dict[str, Any]) -> HourlyPrice:
-        start = (dt.datetime.strptime(record["date"], "%Y-%m-%d") +
-                 dt.timedelta(hours=record["hour"])).astimezone()
-        price_kwh_dkk = float(record["price"])
+        start = dt.datetime.fromisoformat(record["HourDK"]).astimezone()
+        price_kwh_dkk = float(record["Total"])
         return HourlyPrice(start=start, price_kwh_dkk=price_kwh_dkk)
 
     # Sort hourly prices by datetime (first entry is closest to current time)
     hourly_prices = sorted([_convert(r) for r in records], key=lambda p: p.start)
+
+    # Remove hours prior to current hour
+    valid_dt = dt.datetime.now().astimezone() - dt.timedelta(hours=1)
+    hourly_prices = [p for p in hourly_prices if p.start >= valid_dt]
     return hourly_prices
