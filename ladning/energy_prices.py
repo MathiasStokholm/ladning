5258fb4dbe8ec3fd11a8433bc520a7a3d85815dd
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2025-04-01 20:13:21 +0200

    Switched to nrgi.dk for electricity prices

--- ladning/energy_prices.py
+++ ladning/energy_prices.py
@@ -7,25 +7,36 @@ import requests
 
 def get_energy_prices() -> List[HourlyPrice]:
     """
-    Get the energy prices including tariffs and taxes from https://elprisen.somjson.dk.
+    Get the energy prices including tariffs and taxes from Bolius.
 
-    See https://elprisen.somjson.dk/apidocs/#/Elpriser/get_elpris for more info.
+    See https://www.bolius.dk/elpriser for more info.
 
     :return: The hourly energy prices from now until the most recently published price
     """
-    radius_elnet_gln_number = "5790000705689"  # Price area for Sealand and Copenhagen
-    url = f"https://elprisen.somjson.dk/elpris?GLN_Number={radius_elnet_gln_number}"
-    records = requests.get(url).json()["records"]
+    endpoint = "https://nrgi.dk/api/common/pricehistory"
+    now = dt.datetime.now().astimezone()
+
+    price_area = "DK2"  # Price area for Sealand and Copenhagen
+    # The 'includeGrid' option includes tariffs and taxes in the price
+    url = f"{endpoint}?region={price_area}&includeGrid=true"
+    url_today = f"{url}&date={now.strftime('%Y-%m-%d')}"
+    url_tomorrow = f"{url}&date={(now + dt.timedelta(hours=24)).strftime('%Y-%m-%d')}"
+
+    # Download prices for today and tomorrow
+    records = requests.get(url_today).json()["prices"] + requests.get(url_tomorrow).json()["prices"]
+
+    # Exclude predicted prices for now to only use actual prices
+    records = [r for r in records if not r["isPrediction"]]
 
     def _convert(record: Dict[str, Any]) -> HourlyPrice:
-        start = dt.datetime.fromisoformat(record["HourDK"]).astimezone()
-        price_kwh_dkk = float(record["Total"])
+        start = dt.datetime.fromisoformat(record["localTime"]).astimezone()
+        price_kwh_dkk = float(record["totalPriceInclVat"]) * 0.01  # Convert from øre to DKK
         return HourlyPrice(start=start, price_kwh_dkk=price_kwh_dkk)
 
-    # Sort hourly prices by datetime (first entry is closest to current time)
+    # Sort hourly prices by datetime (first entry is earliest)
     hourly_prices = sorted([_convert(r) for r in records], key=lambda p: p.start)
 
-    # Remove hours prior to current hour
-    valid_dt = dt.datetime.now().astimezone() - dt.timedelta(hours=1)
-    hourly_prices = [p for p in hourly_prices if p.start >= valid_dt]
+    # Exclude past times except current hour
+    hourly_prices = [p for p in hourly_prices if p.start > (now - dt.timedelta(hours=1))]
+
     return hourly_prices
