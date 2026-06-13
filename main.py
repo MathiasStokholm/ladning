ab514c451b6beeec18c42cd2e4d7102dd71555a6
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2026-06-13 23:11:05 +0200

    Force usage of TLS v1.3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from typing import AsyncIterator, Tuple, Optional, List
 import datetime as dt
 
+import requests
 from pyeasee import Easee
 import argparse
 import teslapy
@@ -256,6 +257,17 @@ async def schedule_charge(charger: Charger, charging_plan: ChargingPlan) -> None
         raise RuntimeError(f"Scheduling charge failed: '{response.reason}' (code {response.status})")
 
 
+from requests.adapters import HTTPAdapter
+import ssl
+
+class TLSv13Adapter(HTTPAdapter):
+    def init_poolmanager(self, *args, **kwargs):
+        ctx = ssl.create_default_context()
+        ctx.minimum_version = ssl.TLSVersion.TLSv1_3
+        kwargs['ssl_context'] = ctx
+        return super().init_poolmanager(*args, **kwargs)
+
+
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tesla_username", help="The Tesla username to use", required=True, type=str)
@@ -271,6 +283,9 @@ async def main():
 
     # Connect to Tesla API
     tesla = teslapy.Tesla(args.tesla_username)
+    tesla.mount(
+        "https://", TLSv13Adapter(max_retries=tesla.get_adapter("https://").max_retries)
+    )
 
     # Create application state to tie together different pieces of the app
     state = ApplicationState(easee, tesla, get_energy_prices(), args.max_average_price_default)
